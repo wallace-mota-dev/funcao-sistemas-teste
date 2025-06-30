970a372a69366469c50ac6da56a962e25e57366b
Wallace Mota <carvalhowallace117@gmail.com>
2025-06-30 13:05:15 -0300

    #US0001 - Adiciona logica para consultar beneficiario

---
+++ FI.AtividadeEntrevista/DAL/Beneficiarios/DaoBeneficiarios.cs
@@ -0,0 +1,73 @@
+﻿using FI.AtividadeEntrevista.BLL;
+using FI.AtividadeEntrevista.DML;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.DAL
+{
+    internal class DaoBeneficiarios : AcessoDados
+    {
+        /// <summary>
+        /// Inclui novos beneficiários
+        /// </summary>
+        /// <param name="beneficiarios">Objeto de beneficiarios</param>
+        internal long Incluir(List<DML.Beneficiarios> beneficiarios)
+        {
+            long ret = 0;
+
+            foreach (DML.Beneficiarios data in beneficiarios)
+            {
+                List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+                parametros.Add(new System.Data.SqlClient.SqlParameter("Nome", data.Nome));
+                parametros.Add(new System.Data.SqlClient.SqlParameter("CPF", data.CPF));
+                parametros.Add(new System.Data.SqlClient.SqlParameter("IdCliente", data.IdCliente));
+                
+                DataSet ds = base.Consultar("FI_SP_IncBenef", parametros);
+                if (ds.Tables[0].Rows.Count > 0)
+                    long.TryParse(ds.Tables[0].Rows[0][0].ToString(), out ret);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Conculta os beneficiários pelo id do cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        internal List<DML.Beneficiarios> Consultar(long Id)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("IdCliente", Id));
+
+            DataSet ds = base.Consultar("FI_SP_ConsBenef", parametros);
+            List<DML.Beneficiarios> beneficiarios = Converter(ds);
+
+            return beneficiarios;
+        }
+
+        private List<DML.Beneficiarios> Converter(DataSet ds)
+        {
+            List<DML.Beneficiarios> lista = new List<DML.Beneficiarios>();
+            if (ds != null && ds.Tables != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    DML.Beneficiarios beneficiarios = new DML.Beneficiarios();
+                    beneficiarios.Id = row.Field<long>("Id");
+                    beneficiarios.CPF = row.Field<string>("CPF");
+                    beneficiarios.Nome = row.Field<string>("Nome");
+                    beneficiarios.IdCliente = row.Field<long>("IdCliente");
+                    lista.Add(beneficiarios);
+                }
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ FI.AtividadeEntrevista/DAL/Beneficiarios/Procedures/FI_SP_ConsBenef.sql
@@ -0,0 +1,47 @@
+﻿
+ALTER PROC FI_SP_ConsBenef
+    @IDCLIENTE BIGINT = NULL 
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    BEGIN TRY
+        IF @IDCLIENTE IS NOT NULL AND @IDCLIENTE > 0
+        BEGIN
+
+            IF NOT EXISTS (SELECT 1 FROM CLIENTES WHERE ID = @IDCLIENTE)
+            BEGIN
+                RAISERROR('Cliente não encontrado', 16, 1);
+                RETURN;
+            END
+            
+            SELECT 
+                ID,
+                NOME, 
+                CPF, 
+                IDCLIENTE
+            FROM BENEFICIARIOS 
+            WHERE IDCLIENTE = @IDCLIENTE
+            ORDER BY NOME;
+        END
+        ELSE
+        BEGIN
+
+            SELECT 
+                ID,
+                NOME, 
+                CPF, 
+                IDCLIENTE
+            FROM BENEFICIARIOS
+            ORDER BY NOME;
+        END
+        
+    END TRY
+    BEGIN CATCH
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
+        DECLARE @ErrorState INT = ERROR_STATE();
+        
+        RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
+    END CATCH
+END
